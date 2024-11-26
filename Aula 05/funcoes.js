3dae0cb1ff3bb1da30c89d8f730d3aa9637934dc
Carlos <souzacef@gmail.com>
2024-11-26 14:40:55 -0300

    Aula 05

---
+++ Aula 05/funcoes.js
@@ -0,0 +1,23 @@
+const somar = (a,b) => {
+    return a + b;
+}
+
+const subtrair = (a,b) => {
+    return a - b;
+}
+
+const multiplicar = (a,b) => {
+    return a * b;
+}
+
+const dividir = (a, b) => {
+    return a / b;
+}
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+}
+
+
---
+++ Aula 05/index.js
@@ -0,0 +1,22 @@
+const { somar, subtrair, multiplicar, dividir } = require("./funcoes")
+
+
+console.log("Olá, pessoas!");
+
+let resultado = somar(10,5);
+console.log(resultado);
+
+resultado = subtrair(10,5);
+console.log(resultado);
+
+resultado = multiplicar(10,5);
+console.log(resultado);
+
+resultado = dividir(10,5);
+console.log(resultado);
+
+
+
+
+
+
