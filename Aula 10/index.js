14315439b94cb964175be31dea27adb3fc17d128
Carlos <souzacef@gmail.com>
2024-11-27 16:36:48 -0300

    Polimorfismo

--- Aula 10/index.js
+++ Aula 10/index.js
@@ -6,8 +6,8 @@ class Pessoa {
         this.genero = genero
     }
     
-    nomeIdade = function () {
-        return this.nome + " " + this.idade;
+    infos = function () {
+        return this.nome + " " + this.idade + " anos ";
     }
 }
 
@@ -22,8 +22,38 @@ class Professor extends Pessoa{
         this.turmas = turmas,
         this.salario = salario
     }
+
+    infos = function () {
+        return this.nome + " " + this.turmas + " " + this.salario ;
+    }
+
 }
 
-const prof = new Professor("Carlos", 38, 1.80, "M", 16, 100000);
+class Aluno extends Pessoa {
+    constructor(nome, idade, altura, genero, nota, turma, rM) {
+        super(
+            nome, 
+            idade, 
+            altura,
+            genero
+        )
+        this.nota = nota,
+        this.turma = turma,
+        this.rM = rM
+    }
+
+    infos = function () {
+        return this.nome + " " + this.idade + " anos " + this.rM;
+    }
+
+}
+
+const prof = new Professor("Carlos", 38, 1.80, "M", [12,14,16,25], 100000);
+const estudante = new Aluno("Eduardo", 12, 1.60, "M", 10, 16, 123456)
 
 console.log(prof);
+console.log(estudante);
+
+console.log(prof.infos());
+console.log(estudante.infos());
+
