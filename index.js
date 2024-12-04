129be004f304fe28a74a56b651e22febf5c99b2a
souzacef <125619939+souzacef@users.noreply.github.com>
2024-12-04 19:41:40 -0300

    Express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+})
+
+app.get("/contato", (req, res) => {
+    res.send("Nosso contato: email@email.com");
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+})
