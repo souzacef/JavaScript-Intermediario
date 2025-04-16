0d5065888998510e7bb4adf32faf8f16f06d8a92
souzacef <souzacef@gmail.com>
2025-04-16 14:53:01 -0300

    Express

---
+++ Aula 13/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (rec, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/contato", (rec, res) => {
+    res.send("Nosso contato: email@email.com");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);    
+});
