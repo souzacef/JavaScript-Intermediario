c4744fe8075304330aa5b32116124b01b2d383e8
souzacef <souzacef@gmail.com>
2025-04-21 12:22:30 -0300

    Enviando e recebendo informaões

--- Aula 16/controller/empresa.controller.js
+++ Aula 16/controller/empresa.controller.js
@@ -1,5 +1,22 @@
+const empresas = [
+    {
+        id: 1,
+        nome:"empresa 1",
+        numFuncionarios: 100
+    },
+    {
+        id: 2,
+        nome:"empresa 2",
+        numFuncionarios: 1200
+    },
+    {
+        id: 3,
+        nome:"empresa 3",
+        numFuncionarios: 5000
+    }
+]
 const findAllEmpressas = (req,res) => {
-    res.send("todas as empresas listadas");
+    res.send(empresas);
 }
 
 const find = (req,res) => {
